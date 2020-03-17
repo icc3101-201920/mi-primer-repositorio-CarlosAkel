e8ee446e514ba6eb6fb6149de070dc1f1be4985e
CarlosAkel <cmakel@miuandes.cl>
2020-03-16 23:20:18 -0300

    Hi

---
+++ Test.exe/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Test.exe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rotonda angulo = new Rotonda(0,0);
+            angulo.giro = 70;
+            angulo.Fries = 1;
+            angulo.Hamburger = 2;
+            Console.WriteLine(angulo.Get());
+            angulo.Angulo();
+
+
+
+        }
+    }
+}
---
+++ Test.exe/Rotonda.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.exe
+{
+    class Rotonda
+    {
+        public int giro { get; set; }
+        public int Hamburger { get; set; }
+        public int Fries { get; set; }
+
+
+        public void Angulo()
+        {
+            Console.WriteLine("El Angulo de la rotonda es de " + giro + " grados");
+
+
+        }
+
+        public Rotonda( int Hambuger, int Fries)
+        {
+            this.Hamburger = Hamburger;
+            this.Fries = Fries;
+           
+        }
+
+        public string Get()
+        {
+            return $"{Hamburger} and {Fries}";
+
+        }
+    }
+}
